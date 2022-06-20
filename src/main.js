9306b091d94a54d27be4216b87b3a2d6382bb075
Driijko <driijko@gmail.com>
2022-06-19 22:42:48 -0400

    Place open graph image in public folder.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // IMPORTS ////////////////////////////////////////////
 import "../public/global.css";
-import App from './App.svelte'
+import App from './app/App.svelte'
 
 const app = new App({
   target: document.getElementById('app')
