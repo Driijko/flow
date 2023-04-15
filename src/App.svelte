2884732619b5806fe66116cfc6c5efc64b9d7f9d
Driijko <driijko@gmail.com>
2023-04-15 12:23:30 -0400

    General: open graph image.

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,26 @@
 <div>
-  <h1>FLOW</h1>
+  <h1>flow</h1>
 </div>
 
 <style>
   div {
     width: 100%;
-    height: 100%;
+    height: 100vh;
     border: 4px solid red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* background-color: black; */
   }
   h1 {
     font-family: "Monoton";
+    border: 10px solid black;
+    width: 1200px;
+    height: 630px;
+    /* color: white; */
+    font-size: 350px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
