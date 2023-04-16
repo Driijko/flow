d6d68f6b8a30dc8e075f0e1b5cc632151de88912
Driijko <driijko@gmail.com>
2023-04-16 12:20:32 -0400

    Structures: AudioBkgPlayer.

--- src/App.svelte
+++ src/App.svelte
@@ -3,16 +3,10 @@
   // IMPORTS -----------------------------------------------
   import audioBkgStore from "./toolkit/site/audio/bkg/audioBkgStore";
   import AudioBkg from "./toolkit/site/audio/bkg/AudioBkg.svelte";
-  import AudioBkgPausePlayButton from "./toolkit/elements/AudioBkgPausePlayButton.svelte";
-  import AudioBkgVolumeSlider from "./toolkit/elements/AudioBkgVolumeSlider.svelte";
+  import AudioBkgPlayer from "./toolkit/structures/AudioBkgPlayer.svelte";
 
 </script>
 
 <!-- MARKUP //////////////////////////////////////////////// -->
 <AudioBkg />
-<AudioBkgPausePlayButton />
-<AudioBkgVolumeSlider />
-<button type="button" on:click={()=> audioBkgStore.crossFade({path: "./assets/audio/bkg/test-music.mp3",name:"Iroh 0"},3)}>Load</button>
-<p>
-  {$audioBkgStore.track.name}
-</p>
+<AudioBkgPlayer />
---
+++ src/toolkit/structures/AudioBkgPlayer.svelte
@@ -0,0 +1,42 @@
+<!-- SCRIPTS ////////////////////////////////////////// -->
+<script>
+  // IMPORTS -------------------------------------------------
+  import audioBkgStore from "../site/audio/bkg/audioBkgStore";
+  import AudioBkgPausePlayButton from "../elements/AudioBkgPausePlayButton.svelte";
+  import AudioBkgVolumeSlider from "../elements/AudioBkgVolumeSlider.svelte";
+</script>
+
+<!-- MARKUP ///////////////////////////////////////////// -->
+<div class="audio-bkg-player">
+  <div>
+    <h1>{$audioBkgStore.track.name}</h1>
+  </div>
+  <div>
+    <AudioBkgPausePlayButton />
+    <AudioBkgVolumeSlider />
+  </div>
+</div>
+
+<!-- STYLES /////////////////////////////////////////////// -->
+<style>
+.audio-bkg-player {
+  border: 4px solid blue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100px;
+}
+.audio-bkg-player div:nth-of-type(1) {
+  border: 1px solid green;
+  height: 33%;
+  display: flex;
+  align-items: center;
+}
+.audio-bkg-player div:nth-of-type(2) {
+  height: 66%;
+  border: 1px solid blue;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+</style>
