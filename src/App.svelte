be89449ab9965ccb104f8083f727b163fef86640
Driijko <driijko@gmail.com>
2023-04-17 12:37:11 -0400

    Site: setUp & App: runs setUp.

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,20 @@
 <!-- SCRIPTS //////////////////////////////////////////// -->
 <script>
   // IMPORTS -----------------------------------------------
+  import setUp from "./toolkit/1-site/setUp";
   import audioBkgStore from "./toolkit/1-site/audio/bkg/audioBkgStore";
   import resizeStore from "./toolkit/1-site/viewport/resizeStore";
   import AudioBkg from "./toolkit/1-site/audio/bkg/AudioBkg.svelte";
   import AudioBkgPlayer from "./toolkit/5-structures/AudioBkgPlayer.svelte";
   import AudioBkgTrackList from "./toolkit/5-structures/AudioBkgTrackList.svelte";
   import AudioBkgPlaylist from "./toolkit/5-structures/AudioBkgPlaylist.svelte";
+
+  // INITIAL SET UP -------------------------------------------------
+  // This function only needs to be run if you are using
+  // full-viewport layout techniques.
+  setUp();
 </script>
 
 <!-- MARKUP //////////////////////////////////////////////// -->
 <AudioBkg />
-{#key $resizeStore}
-  <AudioBkgPlaylist />
-{/key}
+<AudioBkgPlaylist />
