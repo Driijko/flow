2cb68fb3d371445604dd2dbb0b0682bdb2c279d7
Driijko <driijko@gmail.com>
2023-04-22 14:26:47 -0400

    Pages: OpeningPromptContent.

--- src/App.svelte
+++ src/App.svelte
@@ -5,9 +5,11 @@
   import AudioBkgCheckbox from "./toolkit-new/components/7-elements/interface/audio-bkg/AudioBkgCheckbox.svelte";
   import AudioBkg from "./toolkit-new/components/1-site/AudioBkg.svelte";
   import SiteSettings from "./toolkit-new/components/6-structures/SiteSettings.svelte";
+  import OpeningPromptContent from "./toolkit-new/components/2-pages/opening-prompt/OpeningPromptContent.svelte";
+    import OpeningPromptPage from "./toolkit-new/components/2-pages/opening-prompt/OpeningPromptPage.svelte";
 
   setUp();
 </script>
 <AudioBkg />
-<SiteSettings />
+<OpeningPromptContent />
 <!-- <Router /> -->
---
+++ src/toolkit-new/components/2-pages/opening-prompt/OpeningPromptContent.svelte
@@ -0,0 +1,17 @@
+<!-- SCRIPTS //////////////////////////////////////////// -->
+<script>
+  import SiteSettings from "../../6-structures/SiteSettings.svelte";
+</script>
+
+<!-- MARKUP //////////////////////////////////////////// -->
+<main class="foreground">
+  <p>Welcome to FLOW</p>
+  <p>
+    For the best experience the following settings are recommeded:
+  </p>
+  <SiteSettings />
+  <p>
+    You can change these settings later in the menu.
+  </p>
+  <a on:click|preventDefault href="link">Ready</a>
+</main>
