56068ce41ce95bf8deee9ebe2b2d28426a43defa
Driijko <driijko@gmail.com>
2023-05-05 16:30:04 -0400

    Elements: AnimationsButton.

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,18 @@
   import setUp from "./toolkit/scripts/setUp";
   import Router from "./toolkit/components/1-site/Router.svelte";
   import Notify from "./toolkit/components/3-layers/Notify.svelte";
+  import AnimationsButton 
+  from "./toolkit/components/6-elements/interface/modals/AnimationsButton.svelte";
+  import { animationsOff } from "./toolkit/scripts/animationsStore";
 
   setUp();
 
 </script>
 
 <!-- MARKUP ///////////////////////////////////// -->
-<Router />
+<!-- <Router /> -->
+<AnimationsButton />
+<p>
+  {$animationsOff}
+</p>
 <Notify />
