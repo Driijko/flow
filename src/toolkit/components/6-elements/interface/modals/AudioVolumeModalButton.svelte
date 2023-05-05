e9bb3a6f6141b995361b50c25cfa104a2760355c
Driijko <driijko@gmail.com>
2023-05-05 14:20:14 -0400

    Elements: AudioVolumeModalOpener - no longer needs to react to fading.

--- src/toolkit/components/6-elements/interface/modals/AudioVolumeModalButton.svelte
+++ src/toolkit/components/6-elements/interface/modals/AudioVolumeModalButton.svelte
@@ -2,34 +2,24 @@
 <script>
   // IMPORTS -----------------------------------------
   import { modals } from "../../../../scripts/modalsStore";
-  import { audioBkgVolume, audioBkgFading } 
-  from "../../../../scripts/audioBkgStore";
+  import { audioBkgVolume } from "../../../../scripts/audioBkgStore";
   import AudioVolumeOffIcon from "../../icons/audio/AudioVolumeOffIcon.svelte";
   import AudioVolumeLowIcon from "../../icons/audio/AudioVolumeLowIcon.svelte";
   import AudioVolumeHighIcon from "../../icons/audio/AudioVolumeHighIcon.svelte";
-
-  // STATE ----------------------------------------------
-  let volume = $audioBkgVolume;
-
-  // REACTIVE -----------------------------------------
-  $: if ($audioBkgFading === false) {
-    volume = $audioBkgVolume;
-  };
-
 </script>
 
 <!-- MARKUP /////////////////////////////////////////// -->
 <button type="button" class="audio-bkg-volume-modal-button"
   on:click={()=> modals.toggle("audioVolume")}
-  class:volume-off={volume < 0.03}
-  class:volume-low={volume >= 0.03 && volume < 0.5}
-  class:volume-high={volume >= 0.5}
+  class:volume-off={$audioBkgVolume < 0.03}
+  class:volume-low={$audioBkgVolume >= 0.03 && $audioBkgVolume < 0.6}
+  class:volume-high={$audioBkgVolume >= 0.6}
 >
-  {#if volume < 0.03}
+  {#if $audioBkgVolume < 0.03}
     <AudioVolumeOffIcon title="adjust volume" />
-  {:else if volume >= 0.03 && volume < 0.5}
+  {:else if $audioBkgVolume >= 0.03 && $audioBkgVolume < 0.6}
     <AudioVolumeLowIcon title="adjust volume" />
-  {:else if volume >= 0.5}
+  {:else if $audioBkgVolume >= 0.6}
     <AudioVolumeHighIcon title="adjust volume" />
   {/if}
 </button>
