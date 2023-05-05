52d0f03400724ba5ac6bb9b26ba66f93d57dea21
Driijko <driijko@gmail.com>
2023-05-05 16:55:41 -0400

    Layouts: AudioAnimationList - adjust for user being able to turn off background animations.

--- src/toolkit/scripts/animationsStore.js
+++ src/toolkit/scripts/animationsStore.js
@@ -1,7 +1,7 @@
 import { writable, get } from "svelte/store";
 
 // STATE ---------------------------------
-export const animationsOff = writable(true);
+export const animationsOff = writable(false);
 
 // FUNCTIONS -----------------------------
 export function toggleAnimations() {
