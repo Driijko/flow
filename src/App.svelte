135a97e10666aa279ef15b23b8e5d7ec75e13591
Driijko <driijko@gmail.com>
2023-05-05 15:51:24 -0400

    Notification functionality: notifyStore script, Notify layer component, ResizeNotify layout component, adjust resize script.

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 <script>
   import setUp from "./toolkit/scripts/setUp";
   import Router from "./toolkit/components/1-site/Router.svelte";
+  import Notify from "./toolkit/components/3-layers/Notify.svelte";
 
   setUp();
 
@@ -9,3 +10,4 @@
 
 <!-- MARKUP ///////////////////////////////////// -->
 <Router />
+<Notify />
--- src/toolkit/scripts/viewport/resize.js
+++ src/toolkit/scripts/viewport/resize.js
@@ -2,12 +2,14 @@ import siteSettings from "../../data/siteSettings";
 import viewportCSS from "./viewportCSS";
 import resizeStore from "./resizeStore";
 import viewportOrientationStore from "./viewportOrientationStore";
+import { setNotification } from "../notifyStore";
 
 export default function resize() {
   let resizeReady = false;
   window.addEventListener("resize", ()=> {
     if (resizeReady === false) {
       resizeReady = true;
+      setNotification("resize", siteSettings.resizeDelay);
       const timerId = setTimeout(()=> {
         viewportCSS();
         resizeStore.updateResizeCount();
