bd7028c38b8ffde5b78c2ed002de0027b75c6d88
Driijko <driijko@gmail.com>
2023-05-21 18:42:11 -0400

    global.css: add reset, fundamental layout, universal styling.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,19 @@
 <!-- SCRIPTS //////////////////////////////////////////// -->
 <script>
 
-
-
 </script>
 
 <!-- MARKUP ///////////////////////////////////// -->
+<input type="checkbox" />
 
-
+<!-- STYLES ///////////////////////////////////// -->
+<style>
+  input {
+    border: 4px solid blue;
+    width: 50px;
+    height: 50px;
+  }
+  input:checked {
+    background-color: green;
+  }
+</style>
