5913e4e34e4cb0cb08486d0f87cb618e3c2e8200
Driijko <driijko@gmail.com>
2023-05-21 23:50:02 -0400

    Site: SiteLayout, rough draft.

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import updateOnResize from "./toolkit/scripts/viewport/updateOnResize";
   import { updateLayoutBreakpoint, layoutBreakpoint } 
   from "./toolkit/data/dynamic/layoutBreakpointStore";
+  import SiteLayout from "./components/1-site/SiteLayout.svelte";
 
   updateLayoutBreakpoint();
   viewportCSS();
@@ -12,9 +13,15 @@
 </script>
 
 <!-- MARKUP ///////////////////////////////////// -->
-<p>{$layoutBreakpoint}</p>
+<SiteLayout >
+  <div slot="content">hey</div>
+</SiteLayout>
+
 
 <!-- STYLES ///////////////////////////////////// -->
 <style>
-
+ div {
+  border: 4px solid blue;
+  width: 100%; height: 100%;
+ }
 </style>
---
+++ src/components/1-site/SiteLayout.svelte
@@ -0,0 +1,51 @@
+<!-- SCRIPTS /////////////////////////////////////////// -->
+<script>
+  import { layoutBreakpoint } 
+  from "../../toolkit/data/dynamic/layoutBreakpointStore";
+  import siteSettings from "../../toolkit/data/static/siteSettings";
+</script>
+
+<div class="vp" 
+  class:center={$layoutBreakpoint !== "4K"}
+>
+  <!-- <slot name="vp" /> -->
+
+  {#if $layoutBreakpoint === "mobile"}
+    <div class="pr">
+      <slot name="content" />
+    </div>
+  {:else if $layoutBreakpoint === "HD"}
+    <div class="elr">
+      <slot name="content" />
+    </div>
+  {:else}
+    <div class="interface-area center" 
+      style="min-width:{siteSettings.minPortraitWidth}px;max-width:{siteSettings.maxPortraitWidth}px;"
+    >
+      Interface
+    </div>
+    <div class="content-area center">
+      <div class="elr">content</div>
+    </div>
+  {/if}
+
+</div>
+
+<style>
+  .vp {
+    display: flex;
+  }
+  div {
+    border: 4px solid red;
+  }
+  .interface-area, .content-area {
+    height: 100%;
+    
+  }
+  .interface-area {
+    flex: 2;
+  }
+  .content-area {
+    flex: 1;
+  }
+</style>
