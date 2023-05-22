12b105b1213ef88d588dd1a4e51ef7c4dc4b9d35
Driijko <driijko@gmail.com>
2023-05-21 23:11:24 -0400

    Scripts: add updateLayoutBreakpoint to updateOnResize.

--- src/toolkit/scripts/viewport/updateOnResize.js
+++ src/toolkit/scripts/viewport/updateOnResize.js
@@ -1,9 +1,13 @@
 import viewportCSS from "./viewportCSS";
-import { updateViewportOrientation } from "../../data/dynamic/viewportOrientationStore";
+import { updateViewportOrientation } 
+from "../../data/dynamic/viewportOrientationStore";
+import { updateLayoutBreakpoint } 
+from "../../data/dynamic/layoutBreakpointStore";
 
 export default function updateOnResize() {
   window.addEventListener("resize", ()=> {
     viewportCSS();
     updateViewportOrientation();
+    updateLayoutBreakpoint();
   });
 };
