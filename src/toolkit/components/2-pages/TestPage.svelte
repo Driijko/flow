b46152bc899800ab9af3f2c49a5453ed4415ecf3
Driijko <driijko@gmail.com>
2023-05-23 15:51:10 -0400

    Pages: PageLayout1.

--- src/toolkit/components/2-pages/TestPage.svelte
+++ src/toolkit/components/2-pages/TestPage.svelte
@@ -13,4 +13,13 @@
     {portrait: "./images/test.jpg", 
     landscape:"./images/test2.jpg"}
   }/>
+  <svg slot="pr" viewBox="0 0 900 1600">
+    <defs>
+      <pattern id="pattern1" viewBox="0 0 10 10" width="10%" height="10%">
+        <circle cx="5" cy="5" r="3" fill="white" />
+      </pattern>
+    </defs>
+    <rect x="0" y="0" width="900" height="1600" fill="grey" />
+    <rect x="0" y="0" width="900" height="1600" fill="url(#pattern1)" />
+  </svg>
 </PageLayout1>
--- src/toolkit/components/6-elements/art/FlippableBackground.svelte
+++ src/toolkit/components/6-elements/art/FlippableBackground.svelte
@@ -26,5 +26,6 @@ div {
   width: 100%;
   height: 100%;
   background-size: cover;
+  background-position: 50% 50%;
 }
 </style>
