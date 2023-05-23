7796ba2156f9bafbf5e404f0007294130d8cb776
Driijko <driijko@gmail.com>
2023-05-23 14:19:12 -0400

    Pages: LoadingPage.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,14 @@
 <!-- SCRIPTS //////////////////////////////////////////// -->
 <script>
   import setUp from "./toolkit/scripts/setUp";
+  import Router from "./toolkit/components/1-site/Router.svelte";
 
   setUp();
-  
+
 </script>
 
 <!-- MARKUP ///////////////////////////////////// -->
-
+<Router />
 
 
 
--- src/toolkit/components/1-site/Router.svelte
+++ src/toolkit/components/1-site/Router.svelte
@@ -3,12 +3,13 @@
   // IMPORTS -----------------------------------
   import SiteLayout from "./SiteLayout.svelte";
   import { currentPageName } from "../../data/dynamic/currentPageStore";
+  import LoadingPage from "../2-pages/LoadingPage.svelte";
 
 </script>
 
 <!-- MARKUP ////////////////////////////////////////////// -->
 <SiteLayout>
   {#if $currentPageName === "loading"}
-
+    <LoadingPage />
   {/if}
 </SiteLayout>
